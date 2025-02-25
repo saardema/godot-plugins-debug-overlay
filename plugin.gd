a2560b69cb442fbe88c5b8fdfe42478b0e661186
Sam Aardema <sam.aardema@gmail.com>
2025-02-25 17:39:46 +0100

    Update filepaths

--- plugin.gd
+++ plugin.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 const AUTOLOAD_NAME = "DebugOverlay"
 
 func _enable_plugin():
-	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/debug_overlay/debug_overlay.tscn")
+	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/debug-overlay/debug-overlay.tscn")
 
 func _disable_plugin():
 	remove_autoload_singleton(AUTOLOAD_NAME)
